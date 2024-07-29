b8951c0b8ffb9c70e28bb0331c469950d21d6108
Peppuzzo <giuseppe.43g@libero.it>
2024-07-29 23:38:05 +0200

    Update file
    Update file (2)

--- Database School/SendMateria.php
+++ Database School/SendMateria.php
@@ -38,7 +38,7 @@ $materia = $_POST['materia'];
 /**
  * Connection to the database
  */
-$open = mysqli_connect("localhost", "root", "", "WeDbConnector");
+$open = mysqli_connect("localhost", "root", "", "WebDbConnector");
 
 if ($open === false)
   exit("Error with database connection.") . mysqli_error($open);
